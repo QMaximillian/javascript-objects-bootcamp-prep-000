aa7107d2ce389d4f2f7f614d47d2dbc2499e1313
Quinn Lashinsky <quinnlashinsky@gmail.com>
2017-11-20 23:21:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,3 @@ function updatePlaylist (playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist (playlist, artistName) {
-  delete playlist[artistName] = songTitle
-  return playlist
-}
